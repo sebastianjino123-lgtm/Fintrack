f986414ae85f4a622670df13fc9f4cf32dcddbb8
sebastianjino123-lgtm <sebastianjino123@gmail.com>
2026-05-30 07:34:50 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Increment this version every time you update the app
-const CACHE = 'fintrack-v4';
+const CACHE = 'fintrack-v5';
 const FILES = ['./', './index.html', './manifest.json', './icon.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
