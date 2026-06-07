fbb64ef0b81539f3a2d135944875670ef4711047
sebastianjino123-lgtm <sebastianjino123@gmail.com>
2026-06-07 11:59:32 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Increment this version every time you update the app
-const CACHE = 'fintrack-v26';
+const CACHE = 'fintrack-v27';
 const FILES = ['./', './index.html', './manifest.json', './icon.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
